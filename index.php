ba68e62ec5304ea70242d2c963a1e92e1ddd4cd1
Tamara Mccabe <Tamara Mccabe>
2017-04-23 12:43:18 -0400

    added archive, removed render blocking, added link to github

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require_once "recaptchalib.php";
 
 // your secret key
-$secret = "6Lf3eiMTAAAAAISw5ufT0SOCGb7X2izhxgJfVhVZ";
+$secret = "";
 
 // empty response
 $response = null;
@@ -55,20 +55,27 @@ if ($_POST["g-recaptcha-response"]) {
 			</div><!-- End #scroll-nav -->
 			<div id="slide-nav" ng-class="{'nav-clicked':isNavOpen}" ng-style="{height: navHeightOffset()}">
 				<ul>
-					<li class="raleway"><a href="/#hero-background" ng-click="gotoElement('hero-background', $event)" tabindex="2"><img src="/site15/assets/nav_icon_home.png" alt="home" /><span>Home</span></a></li>
-					<li class="raleway"><a href="/#about-text" tabindex="3" ng-click="gotoElement('about-text', $event)"><img src="/site15/assets/nav_icon_book.png" alt="About" /><span>About</span></a></li>
-					<li class="raleway"><a href="/#skill-text" tabindex="4" ng-click="gotoElement('skill-text', $event)"><img src="/site15/assets/nav_icon_know.png" alt="Development Skills" /><span>Development Skills</span></a></li>
-					<li class="raleway"><a class="disabled" tabindex="5"><img src="/site15/assets/nav_icon_code.png" alt="Code Snippets" /><span>Code Snippets</span></a></li>
-					<li class="raleway"><a href="#" ng-click="showContactForm($event)" tabindex="6"><img src="/site15/assets/nav_icon_mail.png" alt="Contact" /><span>Contact</span></a></li>
-					<li class="raleway"><a href="https://www.linkedin.com/in/joe-prehoda-8040a325" tabindex="7"><img src="/site15/assets/nav_icon_linkedin.png" alt="LinkedIn" /><span>LinkedIn</span></a></li>
+					<li class="raleway"><a href="/#hero-background" ng-click="gotoElement('hero-background', $event)" tabindex="2"><img src="/site15/assets/nav_icon_home.png" alt="" role="presentation" /><span>Home</span></a></li>
+					<li class="raleway"><a href="/#about-text" tabindex="3" ng-click="gotoElement('about-text', $event)"><img src="/site15/assets/nav_icon_book.png" alt="" role="presentation" /><span>About</span></a></li>
+					<li class="raleway"><a href="/#skill-text" tabindex="4" ng-click="gotoElement('skill-text', $event)"><img src="/site15/assets/nav_icon_know.png" alt="" role="presentation" /><span>Development Skills</span></a></li>
+					<li class="raleway"><a href="https://github.com/jprehoda" tabindex="5"><img src="/site15/assets/nav_icon_code.png" alt="" role="presentation" /><span>Code Snippets</span></a></li>
+					<li class="raleway"><a href="#" ng-click="showContactForm($event)" tabindex="6"><img src="/site15/assets/nav_icon_mail.png" alt="" role="presentation" /><span>Contact</span></a></li>
+					<li class="raleway"><a href="https://www.linkedin.com/in/joe-prehoda-8040a325" tabindex="7"><img src="/site15/assets/nav_icon_linkedin.png" alt="" role="presentation" /><span>LinkedIn</span></a></li>
+					<li class="raleway"><a href="#" tabindex="8" ng-class="{active-nav-item: set-nav-item-active}" ng-mouseenter="set-nav-item-active = true" ng-mouseleave="set-nav-item-active = false" ng-click="set-nav-item-active = !set-nav-item-active" ><img src="/site15/assets/nav_icon_linkedin.png" alt="" role="presentation" /><span>Archive</span></a>
+						<ul>
+							<li class="raleway"><a herf="/2009/index.html" tabindex="9">2009 Site</a></li>
+							<li class="raleway"><a herf="/2011/index.html" tabindex="10">2009 Site</a></li>
+						</ul>
+					</li>
+
 				</ul>
 			</div><!-- End #slideNav -->
 		</nav>
 	</header>
 
 	<section id="hero-background" class="hide-tp">
-		<img class="b-lazy" src="/site15/assets/spacer.gif" data-src="/site15/assets/family_hero.jpg|/site15/assets/family_hero_retina.jpg" alt="Joe Prehoda - Family man and front end developer" />
-		<img class="b-lazy alt-hero" src="/site15/assets/spacer.gif" data-src="/site15/assets/solo_hero.jpg|/site15/assets/solo_hero_retina.jpg" alt="The code is out there, it&rsquo;s just a matter of putting it together" />
+		<img src="/site15/assets/spacer.gif" src="/site15/assets/family_hero.jpg" role="presentation" alt="" />
+		<img class="alt-hero" data-src="/site15/assets/solo_hero.jpg" role="presentation" alt="" />
 	</section>
 
 	<section id="contact-form" ng-class="{'show-contact':showContact}">
@@ -80,7 +87,7 @@ if ($_POST["g-recaptcha-response"]) {
 						$mail_to_send_to = "contact@joeprehoda.com";
 						$your_feedbackmail = "noreply@joeprehoda.com";
 
-						echo '<p class="raleway contact-thank-you">Thank you '.$_POST['contact-name'].' for contacting me.<br />I should be contacting you shortly. <br /><a href="#" tabindex="20" ng-click="showContactForm($event)">Close</a><p>';
+						echo '<p class="raleway contact-thank-you">Thank you '.$_POST['contact-name'].' for contacting me.<br />I should be contacting you shortly. <br /><a href="#" tabindex="100" ng-click="showContactForm($event)">Close</a><p>';
 
 						$name = $_POST['contact-name'] ;
 						$email = $_POST['contact-email'] ;
@@ -95,17 +102,17 @@ if ($_POST["g-recaptcha-response"]) {
 				<form ng-submit="contactSubmit($event)" method="post" action="?formSubmit=true" name="contact-joe-prehoda" id="contact-joe-prehoda">
 					<fieldset>
 						<label class="contact-label raleway" for="contact-name" id="contact-name-label">Your Name</label>
-						<input autocomplete="off" class="contact-text-input raleway" name="contact-name" id="contact-name" tabindex="8" type="text" maxlength="50" placeholder="Your Name" required />
+						<input autocomplete="off" class="contact-text-input raleway" name="contact-name" id="contact-name" tabindex="20" type="text" maxlength="50" placeholder="Your Name" required />
 						<label class="contact-label raleway" for="contact-email" id="contact-email-label">Your Email</label>
-						<input autocomplete="off" class="contact-text-input raleway" name="contact-email" id="contact-email" tabindex="9" type="email" maxlength="75" placeholder="Your Email" required />
+						<input autocomplete="off" class="contact-text-input raleway" name="contact-email" id="contact-email" tabindex="21" type="email" maxlength="75" placeholder="Your Email" required />
 						<label class="contact-label raleway" for="contact-subject" id="contact-subject-label">Your Subject</label>
-						<input autocomplete="off" class="contact-text-input raleway" name="contact-subject" id="contact-subject" tabindex="10" type="text" maxlength="100" placeholder="Your Subject" />
+						<input autocomplete="off" class="contact-text-input raleway" name="contact-subject" id="contact-subject" tabindex="22" type="text" maxlength="100" placeholder="Your Subject" />
 						<label class="contact-label raleway" for="contact-message" id="contact-message-label">Your Message</label>
-						<textarea autocomplete="off" class="contact-textarea raleway" name="contact-message" id="contact-message" tabindex="11" placeholder="Your Message..." required ></textarea>
-						<input class="contact-submit-button raleway white-text highlight" type="submit" value="Submit" tabindex="12" />
+						<textarea autocomplete="off" class="contact-textarea raleway" name="contact-message" id="contact-message" tabindex="23" placeholder="Your Message..." required ></textarea>
+						<input class="contact-submit-button raleway white-text highlight" type="submit" value="Submit" tabindex="24" />
 						<div class="relative-element">
 							<p class="captcha-error raleway" ng-class="{'show-captcha-error':showreCaptchaError}">Please verify you are not a robot</p>
-							<div class="g-recaptcha" id="g-recaptcha" data-sitekey="6Lf3eiMTAAAAAAkyNNybRFNfdqO2Va0OsmDrTwIy" data-tabindex="13"></div>
+							<div class="g-recaptcha" id="g-recaptcha" data-sitekey="6Lf3eiMTAAAAAAkyNNybRFNfdqO2Va0OsmDrTwIy" data-tabindex="25"></div>
 						</div>
 					</fieldset>
 				</form>
@@ -124,7 +131,7 @@ if ($_POST["g-recaptcha-response"]) {
 			<h2 class="raleway">A web developer using inspired creativity<br class="hide-dt" /> to engage an audience.</h2>
 
 			<div class="float-wrapper">
-				<div class="nav-bucket" ng-click="gotoElement('about-text', $event)">
+				<div class="nav-bucket" ng-click="gotoElement('about-text', $event)" tabindex="30">
 					<div class="bucket-title highlight wrapper-inset raleway white-text"><h3 class="raleway">My Story</h3></div>
 					<div class="bucket-description wrapper-inset">
 						<p class="raleway">I&rsquo;m a fast-learning front-<br class="hide-tp" />end web developer always on the lookout for new ways to solve complex problems.</p>
@@ -134,7 +141,7 @@ if ($_POST["g-recaptcha-response"]) {
 					</div>
 				</div><!-- End .nav-bucket -->
 
-				<div class="nav-bucket" ng-click="gotoElement('what-i-know-section', $event)">
+				<div class="nav-bucket" ng-click="gotoElement('what-i-know-section', $event)" tabindex="31">
 					<div class="bucket-title highlight wrapper-inset white-text"><h3 class="raleway">What I Know</h3></div>
 					<div class="bucket-description wrapper-inset">
 						<p class="raleway">Specializing in HTML,<br class="hide-tp" />CSS and JavaScript, but these are just a few things that make up my extensive portfolio.</p>
@@ -145,16 +152,15 @@ if ($_POST["g-recaptcha-response"]) {
 				</div><!-- End .nav-bucket -->
 
 				<div id="snippet-bucket" class="nav-bucket hide-tp">
-					<div class="disabled">
-					<div class="bucket-title highlight wrapper-inset raleway white-text"><h3 class="raleway">What I can do</h3></div>
-					<div class="bucket-description wrapper-inset">
-						<p class="raleway">Check out some<br class="hide-tp" />examples of my work or view my test page where I keep all of my code snippets.</p>
-					</div>
-					<div class="bucket-image">
-						<img class="b-lazy blazy-mobile" src="/site15/assets/spacer.gif" data-src="/site15/assets/my_work_icon.png" alt="Code Snippets" />
-					</div>
-					</div><!-- End .disabled -->
-					<p class="coming-soon raleway raleway-bold">Coming Soon</p>
+					<a href="https://github.com/jprehoda" tabindex="32">
+						<div class="bucket-title highlight wrapper-inset raleway white-text"><h3 class="raleway">What I can do</h3></div>
+						<div class="bucket-description wrapper-inset">
+							<p class="raleway">Check out some<br class="hide-tp" />examples of my work or view my test page where I keep all of my code snippets.</p>
+						</div>
+						<div class="bucket-image">
+							<img class="b-lazy blazy-mobile" src="/site15/assets/spacer.gif" data-src="/site15/assets/my_work_icon.png" alt="Code Snippets" />
+						</div>
+					</a>
 				</div><!-- End .nav-bucket -->
 			</div><!-- End .float-wrapper -->
 		</div><!-- End .hero-wrapper -->
@@ -174,7 +180,7 @@ if ($_POST["g-recaptcha-response"]) {
 			<div id="about-text" class="section-text float-right">
 				<h4 class="raleway white-text">A bit about my experience</h4>
 				<p class="raleway white-text">I&rsquo;ve been involved in the graphic design field since the early 2000s and moved over to web development in 2007 at the Naples Daily News. There I was given the chance to wear many hats, among them being web designer and web developer. Working for NDN provided a solid foundation which allowed me to move into a Senior Web Designer role at Sony Electronics. At Sony, I didn't just add to my coding experience, but I also gained knowledge in the business aspects of web design and development. The experience I gained from both Sony and NDN have allowed me to apply those talents today at Chicos as a Front End Developer.</p>
-				<p class="raleway white-text">I’m always on the look out for new opportunities that will provide me a chance to learn and expand my talents in web development. <a href="https://www.linkedin.com/in/joe-prehoda-8040a325" tabindex="14">Feel free to check out my LinkedIn profile</a>.</p>
+				<p class="raleway white-text">I’m always on the look out for new opportunities that will provide me a chance to learn and expand my talents in web development. <a href="https://www.linkedin.com/in/joe-prehoda-8040a325" tabindex="35">Feel free to check out my LinkedIn profile</a>.</p>
 			</div><!-- End .section-text -->
 		</div><!-- End .responsive-wrapper -->
 	</section><!-- End #my-story-section -->
@@ -187,7 +193,7 @@ if ($_POST["g-recaptcha-response"]) {
 			<div id="skill-text" class="section-text float-right">
 				<h4 class="raleway">Languages and Programs I know</h4>
 				<p class="raleway">HTML, XHTML, HTML5, CSS, CSS3, JavaScript, jQuery, Angular, JSON, AJAX, SOAP, XML, REST, Cross-browser compatibilty, Responsive design, Media queries, PHP, Django, .jsp, Git, SEO, ADA compliance, WordPress, Movable Type, Notepad++, Filezilla, YouTube API, Facebook API, Twitter API, Pinterest API, Email creation from scratch, Constant Contact, Wufoo, Survey Monkey, Omniture, Google Analytics, Adobe Photoshop, Adobe Illustrator, Adobe Dreamweaver (No I don’t use the code wizard), Brackets, Jira, Trello, Zendesk, Agile, Scrum.</p>
-				<p class="raleway">I’m a pretty fast learner so if you have a proprietary system or something not in the list above, chances are I’ll pick it up pretty quickly. Interested in what you see here? <a href="#" tabindex="15" ng-click="showContactForm($event)">Drop me a line and let’s chat</a>.</p>
+				<p class="raleway">I’m a pretty fast learner so if you have a proprietary system or something not in the list above, chances are I’ll pick it up pretty quickly. Interested in what you see here? <a href="#" tabindex="36" ng-click="showContactForm($event)">Drop me a line and let’s chat</a>.</p>
 			</div><!-- End .section-text -->
 		</div><!-- End .responsive-wrapper -->
 	</section><!-- End #what-i-know-section -->
@@ -195,9 +201,9 @@ if ($_POST["g-recaptcha-response"]) {
 	<footer class="wrapper_small float-wrapper">
 		<p class="float-left raleway white-text">All trademarks belong to their authoritative owners.</p>
 		<p class="float-right raleway">
-			<a href="#" ng-click="showContactForm($event)" tabindex="35">Contact Me</a>
+			<a href="#" ng-click="showContactForm($event)" tabindex="37">Contact Me</a>
 			<span class="footer-divider white-text">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-			<a href="https://www.linkedin.com/in/joe-prehoda-8040a325" tabindex="36">View My LinkedIn profile</a>
+			<a href="https://www.linkedin.com/in/joe-prehoda-8040a325" tabindex="38">View My LinkedIn profile</a>
 		</p>
 	</footer>
 
